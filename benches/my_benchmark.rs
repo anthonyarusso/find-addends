64bb3c6193756c295ba0a5c18e57b4c521d3fc19
Anthony A Russo <aarusso.exe@outlook.com>
2021-04-24 16:56:11 -0700

    Added fifth parameter (None) to each function, removed unused imports, and changed data variables to pull from the correct files.

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -4,7 +4,6 @@ use revised::{find_addends, use_combos};
 use std::fs::File;
 use std::io;
 use std::io::prelude::*;
-use std::path::Path;
 
 use std::time::Duration;
 
@@ -39,30 +38,37 @@ pub fn benchmark(c: &mut Criterion) {
     let mut aoc = c.benchmark_group("Advent of Code Data");
     aoc.measurement_time(Duration::from_millis(10500));
     let aoc_data = read_values("./data/input.txt".to_string());
-    aoc.bench_function("use_combos", |b| b.iter(|| use_combos(black_box(2020.0), black_box(3), &aoc_data)));
-    aoc.bench_function("find_addends", |b| b.iter(|| find_addends(black_box(2020.0), black_box(3), &aoc_data)));
+    aoc.bench_function("use_combos", |b| b.iter(|| use_combos(black_box(2020.0), black_box(3), &aoc_data, None)));
+    aoc.bench_function("find_addends", |b| b.iter(|| find_addends(black_box(2020.0), black_box(3), &aoc_data, None)));
     aoc.finish();
 
     let mut five = c.benchmark_group("five_addends");
     five.sample_size(50).measurement_time(Duration::from_secs(80));
     let five_data = read_values("./data/five_addends_2020_sum.txt".to_string());
-    five.bench_function("use_combos", |b| b.iter(|| use_combos(black_box(2020.0), black_box(5), &five_data)));
-    five.bench_function("find_addends", |b| b.iter(|| find_addends(black_box(2020.0), black_box(5), &five_data)));
+    five.bench_function("use_combos", |b| b.iter(|| use_combos(black_box(2020.0), black_box(5), &five_data, None)));
+    five.bench_function("find_addends", |b| b.iter(|| find_addends(black_box(2020.0), black_box(5), &five_data, None)));
     five.finish();
 
     let mut test_two = c.benchmark_group("test_two");
     test_two.sample_size(20).measurement_time(Duration::from_secs(500));
     let two_data = read_values("./data/2.txt".to_string());
-    test_two.bench_function("use_combos", |b| b.iter(|| use_combos(black_box(2020.0), black_box(5), &two_data)));
-    test_two.bench_function("find_addends", |b| b.iter(|| find_addends(black_box(2020.0), black_box(5), &two_data)));
+    test_two.bench_function("use_combos", |b| b.iter(|| use_combos(black_box(2020.0), black_box(5), &two_data, None)));
+    test_two.bench_function("find_addends", |b| b.iter(|| find_addends(black_box(2020.0), black_box(5), &two_data, None)));
     test_two.finish();
 
     let mut four_perfects = c.benchmark_group("four_perfects");
     four_perfects.sample_size(10).measurement_time(Duration::from_secs(60));
-    let four_perfects_data = read_values("./data/2.txt".to_string());
-    four_perfects.bench_function("use_combos", |b| b.iter(|| use_combos(black_box(2020.0), black_box(5), &four_perfects_data)));
-    four_perfects.bench_function("find_addends", |b| b.iter(|| find_addends(black_box(2020.0), black_box(5), &four_perfects_data)));
+    let four_perfects_data = read_values("./data/four_perfects_2020_sum.txt".to_string());
+    four_perfects.bench_function("use_combos", |b| b.iter(|| use_combos(black_box(2020.0), black_box(5), &four_perfects_data, None)));
+    four_perfects.bench_function("find_addends", |b| b.iter(|| find_addends(black_box(2020.0), black_box(5), &four_perfects_data, None)));
     four_perfects.finish();
+
+    let mut one_big = c.benchmark_group("one_big");
+    one_big.sample_size(10).measurement_time(Duration::from_secs(80));
+    let one_big_data = read_values("./data/five_addends_1_big_2020_sum.txt".to_string());
+    one_big.bench_function("use_combos", |b| b.iter(|| use_combos(black_box(2020.0), black_box(5), &one_big_data, None)));
+    one_big.bench_function("find_addends", |b| b.iter(|| find_addends(black_box(2020.0), black_box(5), &one_big_data, None)));
+    one_big.finish();
 }
 
 criterion_group!(benches, benchmark);
